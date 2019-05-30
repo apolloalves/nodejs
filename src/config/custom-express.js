b6a0ed8718b73033882966f11f53a37f2a1c027b
apolloalves <apolloapolloalves@gmail.com>
2019-05-30 19:20:54 -0300

    Tratamento de erros 404/500

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -23,4 +23,17 @@ app.use(methodOverride(function (req, res) {
 const rotas = require('../app/rotas/rotas');
 rotas(app);
 
+
+app.use(( req, resp, next) => {
+	return resp.status( 404 ).marko(
+		require('../app/views/base/erros/404.marko')
+	); 
+}); 
+
+// middleware
+app.use(( erro, req, resp, next) => {
+	return resp.status( 500 ).marko(
+		require('../app/views/base/erros/500.marko')
+	); 
+}); 
 module.exports = app;
