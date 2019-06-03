79382c1ceaf3767ad06f85ce3ea817516d41399a
apolloalves <apolloapolloalves@gmail.com>
2019-06-03 14:28:48 -0300

    importacao das libs de autenticacao

---
+++ src/config/sessao-autenticacao.js
@@ -0,0 +1,10 @@
+const uuid = require( 'uuid/v4' ); 
+const sessao = require( 'express-session'); 
+const passport = require( 'passport' ); 
+const LocalStrategy = require( 'passport-local').Strategy; 
+
+
+
+module.exports = ( app ) => {
+	
+}
