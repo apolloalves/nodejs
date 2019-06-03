8e044876a5b7712edf7ec1c7efa9b3f588a71d1b
apolloalves <apolloapolloalves@gmail.com>
2019-06-03 15:30:47 -0300

    Autorizacao

--- src/app/rotas/livro-rotas.js
+++ src/app/rotas/livro-rotas.js
@@ -1,12 +1,19 @@
 
 const LivroControlador = require( '../controladores/livro-controlador.js')
 const livroControlador = new LivroControlador() 
-
 const Livro = require('../modelos/Livro')
+const BaseControlador = require( '../controladores/base-controlador')
 
 module.exports = ( app ) => {
 
     const rotasLivro = LivroControlador.rotas(); 
+    app.use(rotasLivro.autenticadas, function( req, resp, next) {
+        if(req.isAuthenticated()) {
+            next(); 
+        }else {
+            resp.redirect(BaseControlador.rotas().login)
+        }
+    })
    
     app.get(rotasLivro.lista, livroControlador.lista()); 
 
